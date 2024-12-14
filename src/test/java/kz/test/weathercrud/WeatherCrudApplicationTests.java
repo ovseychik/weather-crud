87ff00373bd51fabbd50a5da5c84f1d15cc5fbc1
ovseychik <github@ovseychik.me>
2024-12-14 14:50:14 +0500

    Initial commit

---
+++ src/test/java/kz/test/weathercrud/WeatherCrudApplicationTests.java
@@ -0,0 +1,12 @@
+package kz.test.weathercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherCrudApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
